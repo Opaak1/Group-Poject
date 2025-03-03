432d2e5efec1c40632edb318fd320b86711da721
Opaak1 <yannis.lin@cix.csi.cuny.edu>
2025-03-03 11:31:18 -0500

    Function prototype

--- Group Project.cpp
+++ Group Project.cpp
@@ -2,6 +2,12 @@
 #include <fstream>
 using namespace std;
 
+string calculateLetterGrade(double quizAverageScore);
+double findMinQuiz(double quiz1, double quiz2, double quiz3, double quiz4);
+double findAverageQuizScore(double quiz1, double quiz2, double quiz3, double quiz4);
+double findAverageHWScore(double hw1, double hw2, double hw3, double hw4);
+double findAverageLabScore(double lab1, double lab2, double lab3, double lab4);
+
 int main(){
 
 
